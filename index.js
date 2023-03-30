016a2386c5317146a01ed2a2126e3909d2c3ebe7
sabirnc <mohammedsabir5123@gmail.com>
2023-03-31 03:21:40 +0530

    cors added

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 const express = require("express")
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/user')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin:'https://olx-clone-19bm.onrender.com'
+}))
 
 app.use(express.static('./public/'))
 
